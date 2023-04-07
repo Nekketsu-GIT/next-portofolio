2df819b05f0f232f1294622b9a0b2d6168260a53
Nekketsu-GIT <Github:*1997Gunners05Software31*>
2023-04-07 11:31:49 +0200

    metadata for single article

--- src/app/(website)/blog/[slug]/page.tsx
+++ src/app/(website)/blog/[slug]/page.tsx
@@ -1,12 +1,49 @@
 import Article from '@/components/article/article'
+import { client } from '../../../../../sanity/lib/client'
+
+
+const getArticle = async (slug: string) => {
+
+    const query = `*[_type == "article" && slug.current == $slug][0]{
+        title,
+        publishedAt,
+        mainImage,
+        description,
+        categories,
+        body
+    }`
+    const article = await client.fetch(query, {slug})
+    return article
+}
+
+export async function generateMetadata({ params }: { params: { slug: string } }) {
+
+
+    const article = await getArticle(params.slug);
+    if (!article) {
+      return {
+        title: 'José DACOSTA - IT Engineer & Fullstack Developer - Blog - Article not found',
+        description: 'Article not found',
+        keywords: '',
+      };
+    }
+    return {
+      title: 'José DACOSTA - IT Engineer & Fullstack Developer - Blog - ' + article.title,
+      description: article.description,
+      keywords: article.categories.map((category: any) => category.title).join(', '),
+    };
+  }
 
 const SingleArticle = async ({params} : {params: {slug: string}}) => {
 
+    const article = await getArticle(params.slug);
 
-    const {slug} = params;
+    if (!article) {
+        return <div>Article not found</div>
+    }
 
     return (
-        <Article slug={slug} />
+        <Article {...article} />
     )
 }
 
--- src/components/article/article.tsx
+++ src/components/article/article.tsx
@@ -1,82 +1,35 @@
-"use client"
-
 import styles from './article.module.scss'
 import Image from 'next/image'
 import {PortableText} from '@portabletext/react'
-import { useMemo, useState } from 'react'
 import { ArticleModel } from '@/lib/model'
 import { urlForImage } from '../../../sanity/lib/image'
-import { client } from '../../../sanity/lib/client'
 
 
-const Article =  ({slug} : {slug: string}) => {
+const Article =  ({description, title, mainImage, categories, body, publishedAt} : ArticleModel) => {
 
-    const [article, setArticle] = useState<ArticleModel | null>(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState <Error | null>(null);
-
-
-    useMemo(async () => {
-        try {
-            const article = await getArticle(slug);
-            setArticle(article);
-        } catch (error : any) {
-            setError(error);
-        } finally {
-            setLoading(false);
-        }
-    }, [slug]);
-
-    if (loading) {
-        return <div>Loading...</div>
-    }
-
-    if (error) {
-        return <div>Something went wrong</div>
-    }
-
-    if (!article) {
-        return <div>Article not found</div>
-    }
 
     return (
         <article className={styles.article}>
-            <h1>{article.title}</h1>
+            <h1>{title}</h1>
+            <p>{description}</p>
             <div className={styles.article_image}>
                 <Image
-                    src={urlForImage(article.mainImage)?.url() || ''}
-                    alt={article.title}
+                    src={urlForImage(mainImage)?.url() || ''}
+                    alt={title}
                     fill
 
                 />
             </div>
             <div className={styles.article_meta}>
-                <p>{new Date(article.publishedAt).toLocaleDateString()}</p>
-                <p>{article.categories.map((category:any) => category.title).join(', ')}</p>
+                <p>{new Date(publishedAt).toLocaleDateString()}</p>
+                <p>{categories.map((category:any) => category.title).join(', ')}</p>
             </div>
             <div className={styles.article_body}>
-                <PortableText value={article.body} />
+                <PortableText value={body} />
             </div>
         </article>
     )
 }
-
-const getArticle = async (slug: string) => {
-
-
-
-
-    const query = `*[_type == "article" && slug.current == $slug][0]{
-        title,
-        publishedAt,
-        mainImage,
-        categories,
-        body
-    }`
-    const article = await client.fetch(query, {slug})
-    return article
-}
-
 
 export default Article
     
