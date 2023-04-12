5d53d2fb736ba4c143ffc15f2e06cfe3683759e2
José Dacosta <jose@bilik.fr>
2023-04-12 09:56:37 +0200

    apply container everywhere

--- src/components/article/article.tsx
+++ src/components/article/article.tsx
@@ -3,31 +3,34 @@ import Image from 'next/image'
 import {PortableText} from '@portabletext/react'
 import { ArticleModel } from '@/lib/model'
 import { urlForImage } from '../../../sanity/lib/image'
+import BodyContainer from '../BodyContainer/BodyContainer'
 
 
 const Article =  ({description, title, mainImage, categories, body, publishedAt} : ArticleModel) => {
 
 
     return (
-        <article className={styles.article}>
-            <h1>{title}</h1>
-            <p>{description}</p>
-            <div className={styles.article_image}>
-                <Image
-                    src={urlForImage(mainImage)?.url() || ''}
-                    alt={title}
-                    fill
+        <BodyContainer>
+            <article className={styles.article}>
+                <h1>{title}</h1>
+                <p>{description}</p>
+                <div className={styles.article_image}>
+                    <Image
+                        src={urlForImage(mainImage)?.url() || ''}
+                        alt={title}
+                        fill
 
-                />
-            </div>
-            <div className={styles.article_meta}>
-                <p>{new Date(publishedAt).toLocaleDateString()}</p>
-                <p>{categories.map((category:any) => category.title).join(', ')}</p>
-            </div>
-            <div className={styles.article_body}>
-                <PortableText value={body} />
-            </div>
-        </article>
+                    />
+                </div>
+                <div className={styles.article_meta}>
+                    <p>{new Date(publishedAt).toLocaleDateString()}</p>
+                    <p>{categories.map((category:any) => category.title).join(', ')}</p>
+                </div>
+                <div className={styles.article_body}>
+                    <PortableText value={body} />
+                </div>
+            </article>
+        </BodyContainer>
     )
 }
 
