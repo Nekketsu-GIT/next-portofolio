0fc7a62385a33c8a0c3d205e605cc5262ccc568b
Jose DACOSTA <jose.dacosta@square-sense.com>
2025-02-27 12:12:59 +0100

    migrate from sanity to contentlayer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,35 +3,41 @@ import ServiceCard from "@/components/service-card";
 import Title from "@/components/title";
 import Image from "next/legacy/image";
 import BlogCard from "@/components/blog-card";
-import { compareDesc } from 'date-fns'
-import { allPosts } from 'contentlayer/generated'
+import { compareDesc } from "date-fns";
+import { allPosts } from "contentlayer/generated";
 import ProjectCard from "@/components/project-card";
 import KPI from "@/components/kpi";
 
 const projects = [
   {
     title: "Jaay",
-    summary: "This project showcases a fully functional e-commerce website designed for selling clothing. Built with a focus on user experience and responsiveness, the platform features an intuitive interface that ensures seamless navigation across devices. Key functionalities include:",
+    summary:
+      "This project showcases a fully functional e-commerce website designed for selling clothing. Built with a focus on user experience and responsiveness, the platform features an intuitive interface that ensures seamless navigation across devices. Key functionalities include:",
     tags: ["React", "Next.js"],
     preview: "http://jaay.vercel.app/",
     image: "/images/jaay.webp",
   },
   {
     title: "This awesome portofolio",
-    summary: "This project is a personal portfolio website designed to showcase skills, projects, and achievements in a visually appealing and interactive format. The website serves as a digital resume, providing potential clients, employers, and collaborators with easy access to information about my work, expertise, and professional background. The layout is crafted to highlight key projects and skills, while also offering a personal touch that reflects my unique style and approach.",
+    summary:
+      "This project is a personal portfolio website designed to showcase skills, projects, and achievements in a visually appealing and interactive format. The website serves as a digital resume, providing potential clients, employers, and collaborators with easy access to information about my work, expertise, and professional background. The layout is crafted to highlight key projects and skills, while also offering a personal touch that reflects my unique style and approach.",
     tags: ["Node.js", "Express"],
     sourceCode: "https://github.com/Nekketsu-GIT/next-portofolio",
     image: "/images/portfolio.webp",
-  }, {
+  },
+  {
     title: "Travel agency homepage design",
-    summary: "This project features a visually appealing and user-friendly homepage design for a travel agency website. The design focuses on delivering an engaging experience to potential customers, highlighting key services such as travel packages, flight bookings, and customer support. The layout prioritizes intuitive navigation and encourages user interaction, leading to increased conversions and customer engagement.",
+    summary:
+      "This project features a visually appealing and user-friendly homepage design for a travel agency website. The design focuses on delivering an engaging experience to potential customers, highlighting key services such as travel packages, flight bookings, and customer support. The layout prioritizes intuitive navigation and encourages user interaction, leading to increased conversions and customer engagement.",
     tags: ["UI/UX", "HTML", "CSS"],
     sourceCode: "https://github.com/Nekketsu-GIT/Travel-Agency-Udemy",
     preview: "https://jose-voyage.netlify.app/",
     image: "/images/travel.webp",
-  }, {
+  },
+  {
     title: "Sentiment analysis with Python",
-    summary: "This project is a Python-based sentiment analysis tool designed to determine the emotional tone behind text data. It leverages Natural Language Processing (NLP) techniques to classify sentiments as positive, negative, or neutral. The application can be used for various tasks such as analyzing customer feedback, social media posts, or reviews.",
+    summary:
+      "This project is a Python-based sentiment analysis tool designed to determine the emotional tone behind text data. It leverages Natural Language Processing (NLP) techniques to classify sentiments as positive, negative, or neutral. The application can be used for various tasks such as analyzing customer feedback, social media posts, or reviews.",
     tags: ["Python", "NLP"],
     sourceCode: "https://github.com/Nekketsu-GIT/django_sentiment_analysis",
     image: "/images/sentiments.webp",
@@ -39,8 +45,9 @@ const projects = [
 ];
 
 export default async function Home() {
-
-  const posts = allPosts.sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)))
+  const posts = allPosts.sort((a, b) =>
+    compareDesc(new Date(a.date), new Date(b.date))
+  );
 
   const services = [
     {
@@ -75,7 +82,6 @@ export default async function Home() {
     },
   ];
 
-
   return (
     <>
       <Presentation
@@ -89,17 +95,18 @@ export default async function Home() {
         }}
       />
 
-      <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 w-full bg-yaleblue text-white py-8 rounded-lg">
-        {Number(process.env.NEXT_PUBLIC_YEARS_OF_EXPERIENCE) && (
-          <KPI text="Years of experience" value={Number(process.env.NEXT_PUBLIC_YEARS_OF_EXPERIENCE)} />
-        )}
-        {Number(process.env.NEXT_PUBLIC_PROJECTS_COMPLETED) && (
-          <KPI text="Projects completed" value={Number(process.env.NEXT_PUBLIC_PROJECTS_COMPLETED)} />
-        )}
-        {Number(process.env.NEXT_PUBLIC_CLIENTS) && <KPI text="Clients" value={Number(process.env.NEXT_PUBLIC_CLIENTS)} />}
-        {Number(process.env.NEXT_PUBLIC_REVIEWS) && <KPI text="Reviews" value={Number(process.env.NEXT_PUBLIC_REVIEWS)} />}
+      <section className="flex flex-col md:flex-row gap-8 w-full items-center justify-around bg-yaleblue text-white py-8">
+        <KPI
+          text="Years of experience"
+          value={process.env.NEXT_PUBLIC_YEARS_OF_EXPERIENCE}
+        />
+        <KPI
+          text="Projects completed"
+          value={process.env.NEXT_PUBLIC_PROJECTS_COMPLETED}
+        />
+        <KPI text="Clients" value={process.env.NEXT_PUBLIC_CLIENTS} />
+        <KPI text="Reviews" value={process.env.NEXT_PUBLIC_REVIEWS} />
       </section>
-
 
       <section className="py-8 w-full flex flex-col gap-8">
         <Title
@@ -142,7 +149,6 @@ export default async function Home() {
           ))}
         </div>
       </section>
-
 
       <section className="py-8 w-full flex flex-col gap-8">
         <Title
--- src/components/kpi.tsx
+++ src/components/kpi.tsx
@@ -1,17 +1,10 @@
-"use client";
-
-import CountUp from "react-countup";
-
-export default function KPI({ text, value }: { text: string; value: number }) {
+export default function KPI({ text, value }: { text: string; value?: string }) {
   return (
     value && (
       <div className="flex flex-col items-center justify-center">
-        <CountUp
-          end={value}
-          duration={2}
-          className="text-2xl font-bold text-darkgoldenrod"
-        />
-        {/* <h2 className="text-2xl font-bold text-darkgoldenrod">{value}</h2> */}
+        <span className="text-2xl font-bold text-darkgoldenrod animate-pulse">
+          {value}
+        </span>
         <p>{text}</p>
       </div>
     )
